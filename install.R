d153bceff2efca7a8f21ef6960b95ba1ea4d856d
scallis24 <scallis@unc.edu>
2025-08-25 18:25:23 -0400

    Update install.R

--- install.R
+++ install.R
@@ -3,3 +3,4 @@ options(repos = c(CRAN = "https://cran.r-project.org"))
 if (!requireNamespace("readr", quietly = TRUE)) {
   install.packages("readr")
 }
+install.packages(c("readr", "tidyr", "ggplot2"))
