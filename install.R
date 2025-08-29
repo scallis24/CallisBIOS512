e14173a2fceb57c7b5f305008eb91d18ead55750
scallis24 <scallis@unc.edu>
2025-08-29 10:57:41 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 options(repos = c(CRAN = "https://cran.r-project.org"))
 
-if (!requireNamespace("readr", quietly = TRUE)) {
-  install.packages("readr")
+# Install tidyverse only if it's not already available
+if (!requireNamespace("tidyverse", quietly = TRUE)) {
+  install.packages("tidyverse")
 }
-install.packages(c("readr", "tidyr", "ggplot2"))
