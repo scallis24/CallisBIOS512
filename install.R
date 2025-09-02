d06b40f526cf51f5644739b820f11932b6c02327
scallis24 <scallis@unc.edu>
2025-09-02 12:07:30 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,9 @@
+install.packages("readr")
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
+
+options(repos = c(CRAN = "https://cran.r-project.org"))
+install.packages(c("readr","ggplot2","tidyr","shiny"))
