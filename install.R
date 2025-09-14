661968d2f01d1c498fe870b1cb47ef78307b6c22
scallis24 <scallis@unc.edu>
2025-09-13 22:20:55 -0400

    Update install.R

--- install.R
+++ install.R
@@ -3,6 +3,3 @@ install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("skimr", dependencies = TRUE)
 IRkernel::installspec(user = FALSE)
-
-system("wget https://github.com/quarto-dev/quarto-cli/releases/download/v2.12.0/quarto-2.12.0-linux-amd64.deb")
-system("sudo dpkg -i quarto-2.12.0-linux-amd64.deb")
