7712084a48685e9e3136fe39a0bb206c88b50edd
scallis24 <scallis@unc.edu>
2025-09-13 21:56:58 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,7 +1,8 @@
-install.packages("readr")
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
-install.packages("shiny"), dependencies = TRUE)
-
+install.packages("skimr", dependencies = TRUE)
 IRkernel::installspec(user = FALSE)
+
+system("wget https://github.com/quarto-dev/quarto-cli/releases/download/v2.12.0/quarto-2.12.0-linux-amd64.deb")
+system("sudo dpkg -i quarto-2.12.0-linux-amd64.deb")
